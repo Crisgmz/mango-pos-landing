43a483720ede771dede928207ebac5c4b4e943b7
Cristian Gómez <cristiangomez@MacBook-Pro-de-Cristian.local>
2026-03-16 18:11:55 -0400

    remove features teaser from home

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,8 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import LogosStrip from "@/components/LogosStrip";
-import FeaturesSection from "@/components/FeaturesSection";
 import BenefitsSection from "@/components/BenefitsSection";
 import FiscalSection from "@/components/FiscalSection";
 import PricingSection from "@/components/PricingSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import FinalCTA from "@/components/FinalCTA";
 import Footer from "@/components/Footer";
 
@@ -14,12 +11,9 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
-      <LogosStrip />
-      <FeaturesSection />
       <BenefitsSection />
       <FiscalSection />
       <PricingSection />
-      <TestimonialsSection />
       <FinalCTA />
       <Footer />
     </div>
